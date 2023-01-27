6c31f15fbafae44bad23504c3e2fb38888a16415
ashtenwarman <116308024+ashtenwarman@users.noreply.github.com>
2023-01-26 20:17:40 -0600

    Add files via upload

--- mysql-java/src/main/java/projects/ProjectsApp.java
+++ mysql-java/src/main/java/projects/ProjectsApp.java
@@ -12,17 +12,19 @@ import projects.service.ProjectService;
 public class ProjectsApp {
 	//@formatter:off
 	//creating menu
-	private List<String> operations = List.of("1) Add a project");
+	private List<String> operations = List.of("1) Add a project", "2) List projects", "3) Select a project");
 	//@formatter:on
-	//scanner to take inputs
+	// scanner to take inputs
 	private Scanner sc = new Scanner(System.in);
-	//creating Project Service object
+	// creating Project Service object
 	ProjectService projectService = new ProjectService();
-
+	private Project curProject;
+	
 	public static void main(String[] args) {
 		new ProjectsApp().processUserSelections();
 	}
-	//method to process inputs, and check if it is a valid selection
+
+	// process inputs, and check if they are valid
 	private void processUserSelections() {
 		boolean done = false;
 
@@ -35,8 +37,16 @@ public class ProjectsApp {
 					done = exitMenu();
 					break;
 
-				case (1):
+				case 1:
 					createProject();
+					break;
+					
+				case 2:
+					listProjects();
+					break;
+					
+				case 3:
+					selectProject();
 					break;
 
 				default:
@@ -49,12 +59,32 @@ public class ProjectsApp {
 		}
 
 	}
-	//method to exit menu and terminate program
+	private void selectProject() {
+		listProjects();
+		//getting project ID input
+		Integer projectId = getIntInput("Enter a project ID to select a project");
+		curProject = null;
+		//setting current project to ID input
+		curProject = projectService.fetchProjectById(projectId);
+		
+	}
+
+	//printing list of projects
+	private void listProjects() {
+		List<Project> projects = projectService.fetchAllProjects();
+		System.out.println("\nProjects:");
+		projects.forEach(
+				project -> System.out.println("   " + project.getProjectId() + ": " + project.getProjectName()));
+
+	}
+
+	// exit menu and terminate program
 	private boolean exitMenu() {
 		System.out.println("Exiting the menu.");
 		return true;
 	}
-	//method to create a project with given inputs
+
+	//create a project with given inputs
 	private void createProject() {
 		String projectName = getStringInput("Enter the project name");
 		BigDecimal estimatedHours = getDecimalInput("Enter the estimated hours");
@@ -62,8 +92,8 @@ public class ProjectsApp {
 		Integer difficulty = getIntInput("Enter the project difficulty (1-5)");
 		String notes = getStringInput("Enter the project notes");
 		Project project = new Project();
-		
-		//setters
+
+		// setters
 		project.setProjectName(projectName);
 		project.setEstimatedHours(estimatedHours);
 		project.setActualHours(actualHours);
@@ -74,7 +104,8 @@ public class ProjectsApp {
 		System.out.println("You have successfully created project: " + dbProject);
 
 	}
-	//converts input to a BigDecimal
+
+	// converts input to a BigDecimal
 	private BigDecimal getDecimalInput(String prompt) {
 		String input = getStringInput(prompt);
 		if (Objects.isNull(input)) {
@@ -86,13 +117,15 @@ public class ProjectsApp {
 			throw new DbException(input + " is not a valid decimal number.");
 		}
 	}
-	//prints menu and gets user selection for menu input
+
+	// prints menu and gets user selection for menu input
 	private int getUserSelection() {
 		printOperations();
 		Integer input = getIntInput("Enter a menu selection: ");
 		return Objects.isNull(input) ? -1 : input;
 	}
-	//converts input to Integer
+
+	// converts input to Integer
 	private Integer getIntInput(String prompt) {
 		String input = getStringInput(prompt);
 		if (Objects.isNull(input)) {
@@ -104,18 +137,25 @@ public class ProjectsApp {
 			throw new DbException(input + " is not a valid number.");
 		}
 	}
-	//converts input to String
+
+	// converts input to String
 	private String getStringInput(String prompt) {
 		System.out.print(prompt + ": ");
 		String input = sc.nextLine();
 		return input.isBlank() ? null : input.trim();
 
 	}
-	//prints instructions for menu and available selections
+
+	// prints instructions for menu and available selections
 	private void printOperations() {
 		System.out.println("\nThese are the available selections, or press Enter to exit");
 		operations.forEach(line -> System.out.println("   " + line));
 
+		if(Objects.isNull(curProject)) {
+			System.out.println("\nYou are not working with a project.");
+		} else {
+			System.out.println("\nYou are working with project: " + curProject);
+		}
 	}
 
 }
