3061a1eba1d4c8a68ea3e949945fcfa8391bcc47
ashtenwarman <116308024+ashtenwarman@users.noreply.github.com>
2023-02-01 23:03:01 -0600

    Add files via upload

--- mysql-java/src/main/java/projects/dao/ProjectDao.java
+++ mysql-java/src/main/java/projects/dao/ProjectDao.java
@@ -25,7 +25,7 @@ public class ProjectDao extends DaoBase {
 	private static final String PROJECT_TABLE = "project";
 	private static final String PROJECT_CATEGORY_TABLE = "project_category";
 	private static final String STEP_TABLE = "step";
-	//method to write SQL code for new Project inputs
+	//writes SQL code for new Project inputs
 	public Project insertProject(Project project) {
 		//@formatter:off
 		String sql = ""
@@ -62,7 +62,7 @@ public class ProjectDao extends DaoBase {
 		}
 
 	}
-	//method to retrieve list of project names
+	//retrieve list of project names
 	public List<Project> fetchAllProjects() {
 		String sql = "SELECT * FROM " + PROJECT_TABLE + " ORDER BY project_name";
 		
@@ -88,7 +88,7 @@ public class ProjectDao extends DaoBase {
 			throw new DbException(e);
 		}
 	}
-	//method to retrieve all project info once selected 
+	//retrieve selected project info
 	public Optional<Project> fetchProjectById(Integer projectId) {
 		String sql = "SELECT * FROM " + PROJECT_TABLE + " WHERE project_id = ?";
 		
@@ -123,7 +123,7 @@ public class ProjectDao extends DaoBase {
 			throw new DbException(e);
 		}
 	}
-	//method to retrieve step info for a project
+	//retrieve step info for a project
 	private List<Step> fetchStepsForProject(Connection conn, Integer projectId) throws SQLException {
 		String sql = "SELECT * FROM " + STEP_TABLE + " WHERE project_id = ?";
 		
@@ -140,7 +140,7 @@ public class ProjectDao extends DaoBase {
 			}
 		}
 	}
-	//method to retrieve category info for a project
+	//retrieve category info for a project
 	private List<Category> fetchCategoriesForProject(Connection conn, Integer projectId) throws SQLException {
 		//@formatter:off
 		String sql = ""
@@ -162,7 +162,7 @@ public class ProjectDao extends DaoBase {
 		}
 		}
 	}
-	//method to retrieve material info for a project
+	//retrieve material info for a project
 	private List<Material> fetchMaterialsForProject(Connection conn, Integer projectId) throws SQLException {
 		String sql = "SELECT * FROM " + MATERIAL_TABLE + " WHERE project_id = ?";
 		try(PreparedStatement stmt = conn.prepareStatement(sql)) {
@@ -178,5 +178,64 @@ public class ProjectDao extends DaoBase {
 			}
 		}
 	}
-
+	public boolean modifyProjectDetails(Project project){
+		//@formatter:off
+		String sql = ""
+			+ "UPDATE " + PROJECT_TABLE + " SET "
+			+ "project_name = ?, "
+			+ "estimated_hours = ?, "
+			+ "actual_hours = ?, "
+			+ "difficulty = ?, "
+			+ "notes = ? "
+			+ "WHERE project_id = ?";
+		//@formatter:on
+		
+		try(Connection conn = DbConnection.getConnection()) {
+			startTransaction(conn);
+			
+			try(PreparedStatement stmt = conn.prepareStatement(sql)) {
+				setParameter(stmt, 1, project.getProjectName(), String.class);
+				setParameter(stmt, 2, project.getEstimatedHours(), BigDecimal.class);
+				setParameter(stmt, 3, project.getActualHours(), BigDecimal.class);
+				setParameter(stmt, 4, project.getDifficulty(), Integer.class);
+				setParameter(stmt, 5, project.getNotes(), String.class);
+				setParameter(stmt, 6, project.getProjectId(), Integer.class);
+				
+				boolean modified = stmt.executeUpdate() == 1;
+				commitTransaction(conn);
+				
+				return modified;
+			}
+			catch(Exception e) {
+				rollbackTransaction(conn);
+				throw new DbException(e);
+			}
+		} 
+			catch (SQLException e) {
+				throw new DbException();
+			}
+	}
+	public boolean deleteProject(Integer projectId) {
+		String sql = "DELETE FROM " + PROJECT_TABLE + " WHERE project_id = ?";
+		
+		try(Connection conn = DbConnection.getConnection()) {
+			startTransaction(conn);
+			
+			try(PreparedStatement stmt = conn.prepareStatement(sql)) {
+				setParameter(stmt, 1, projectId, Integer.class);
+				
+				boolean deleted = stmt.executeUpdate() == 1;
+				
+				commitTransaction(conn);
+				return deleted;
+			}
+			catch(Exception e) {
+				rollbackTransaction(conn);
+				throw new DbException(e);
+		}		
+		}
+			catch(SQLException e) {
+				throw new DbException(e);
+			}
+	}
 }
